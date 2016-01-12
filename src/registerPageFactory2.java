08bad44da445fda63c2b0ace27688d3383b49716
PratyushJaiswal <Pratyush.jaiswal@softwaysolutions.com>
2016-01-12 12:15:27 +0530

    Comit with removing system jar files

---
+++ src/registerPageFactory2.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+public class registerPageFactory2 {
+	WebDriver driver;
+	
+	@FindBy(xpath = ".//*[@id='license_pop_close']/img")
+	WebElement name_of_element_in_or_text_file;
+	
+	public registerPageFactory2(WebDriver driver){
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+		
+		//Here will be methods name which we are going to call in the framework classes for testing 
+		
+		/*public void methodName(String name){
+			name_of_element_in_or_text_file.sendKeys(name);
+		}*/
+		
+	}
+
+}
