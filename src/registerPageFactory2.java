da2f7f55b1dd6f0497aab0dff9a3773a915e62b4
PratyushJaiswal <Pratyush.jaiswal@softwaysolutions.com>
2016-01-12 12:18:45 +0530

    comit and push

--- src/registerPageFactory2.java
+++ src/registerPageFactory2.java
@@ -11,13 +11,13 @@ public class registerPageFactory2 {
 	public registerPageFactory2(WebDriver driver){
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
-		
+	}
 		//Here will be methods name which we are going to call in the framework classes for testing 
 		
 		/*public void methodName(String name){
 			name_of_element_in_or_text_file.sendKeys(name);
 		}*/
 		
-	}
+	
 
 }
