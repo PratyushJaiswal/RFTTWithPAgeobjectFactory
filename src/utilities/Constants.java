f6ac9f93ccfb0328f3ee56cc304c7dc49243dbc4
PratyushJaiswal <Pratyush.jaiswal@softwaysolutions.com>
2016-02-03 14:35:22 +0530

    comit and push for excel reading

---
+++ src/utilities/Constants.java
@@ -0,0 +1,7 @@
+package utilities;
+
+public class Constants {
+      public static final String URL = "http://www.expedia.com";
+      public static final String File_Path = "//Users//tomara//Documents//workspace//SeleniumWD2Tutorial//src//utilities//";
+      public static final String File_Name = "ExcelData.xlsx";
+}
